5d826fe6b6f11444accc3d45f567cfe4d7738920
jonathannadeau.jn <jonathannadeau.jn@gmail.com>
2022-05-19 21:03:19 -0400

    ajout du bouton click me

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -5,15 +5,20 @@ import CardTitle from './components/CardTitle/CardTitle';
 import CardDescription from './components/CardDescription/CardDescription';
 import CardInfo from './components/CardInfo/CardInfo';
 import CardFooter from './components/CardFooter/CardFooter';
+import { Button } from './components/Button/Button';
 
 function App() {
+  const handleButtonClick = (event) => {
+    alert('Button clicked!');
+  };
   return (
     <div className='app'>
       <Card>
         <CardImage src={'images/image-equilibrium.jpg'} alt={'equilibrium'} />
         <CardTitle title={'Equilibrium #3429'} />
+        <Button textField={'Click me'} onClick={handleButtonClick} />
         <CardDescription description={'Our Equilibrium collection promotes balance and calm.'} />
-        <CardInfo value={'0.0041'} timeLeft={'3 days'} />
+        <CardInfo value={'0.0-41'} timeLeft={'3 days'} />
         <CardFooter src={'images/image-avatar.png'} name={'Jules Wyvern'} />
       </Card>
     </div>
